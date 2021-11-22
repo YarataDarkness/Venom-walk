5883a8183e62068364efc9fb9c4e5cd23d15fa7c
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2021-11-22 14:12:20 -0500

    correction

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const isMarchePause = document.querySelector('.btn');
 const isanimation = document.querySelector('.animation');
 
 isMarchePause.addEventListener('click', function(){
-    isanimation.classList.add("is-walking");
+    isanimation.classList.add('is-walking');
     //isMarchePause.innerHTML = "Pause";
     //console.log(isanimation);
 });
